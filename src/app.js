855765e62821daede9a71a44b5d0867aba8eda7a
Pawel Lipinski <pawel.lipinski@pragmatists.pl>
2016-06-15 17:31:14 +0200

    little cleanup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 var converter = require('./converter.js');
 var generator = require('./generator.js');
 var Datafy = require('./datafy-api.js');
-var Q = require('q');
 
-Datafy.datasource.data = converter.convert();
-
-Q.fcall(Datafy.authenticate)
+Datafy.authenticate()
     .then(Datafy.deleteDS)
     .then(Datafy.createDS)
     .then(generator.generate)
